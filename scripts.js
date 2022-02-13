12f49b864ea191c0bb480b5016b02112834095d3
Kevin De La Torre <61712433+sebtheedark@users.noreply.github.com>
2022-02-12 16:49:42 -0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("#bttn");
+const content = document.createElement("div");
+content.classList.add("content");
+
+button.appendChild(content);
+button.addEventListener("click", () => {
+  bttn.textContent = "Will you be my valentine?";
+  var x = document.createElement("BR");
+});
+
